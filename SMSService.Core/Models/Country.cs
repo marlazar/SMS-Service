66b354542f79d9adfd877d747113473c38f37176
Lazar Markovic <lmarkovicbb@gmail.com>
2022-07-25 21:34:43 +0200

    Add project files.

---
+++ SMSService.Core/Models/Country.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using ServiceStack.DataAnnotations;
+using System.Text;
+
+namespace SMSService.Core.Models
+{
+    internal class Country
+    {
+        [AutoId]
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        [Unique]
+        public string MobileCountryCode { get; set; }
+        [Unique]
+        public string CountryCode { get; set; }
+        public decimal PricePerSMS { get; set; }
+    }
+}
---
+++ SMSService.Core/Models/SMS.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using ServiceStack.DataAnnotations;
+using System.Text;
+
+namespace SMSService.Core.Models
+{
+    public class SMS
+    {
+        [AutoId]
+        public Guid Id { get; set; }
+        [Required]
+        public string Sender { get; set; }
+        [Required]
+        public string Receiver { get; set; }
+        [StringLength(StringLengthAttribute.MaxText)]
+        public string Text { get; set; }
+        public DateTime SendDate { get; set; }
+        public string MobileCountryCode { get; set; }
+    }
+}
